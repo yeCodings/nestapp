42e1c42647ee583acad213a37c460158706db333
yeCodings <yecoding@163.com>
2024-03-07 16:44:16 +0800

    feat:注入范围

--- src/modules/example/services/seventh.service.ts
+++ src/modules/example/services/seventh.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Scope } from '@nestjs/common';
 
 /**
  * 在注入提供者时有多种方法
@@ -6,7 +6,7 @@ import { Injectable } from '@nestjs/common';
  * REQUEST: 每次请求创建一个新的提供者实例，并且该实例在这次请求内被所有调用者所共享，请求完毕自动销毁
  * TRANSIENT：每次被使用者注入该提供者时都会创建一个新的实例，但之后的请求所有注入者都只延用各自已创建的实例了
  */
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class SeventhService {
     protected demo = 0;
 
